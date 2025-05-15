2d98eb4e51ae237b20223e27b8d114577dcc2c31
Max <naturale@hufs.ac.kr>
2025-05-15 14:15:23 +0900

    add style

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,3 +1,3 @@
-from .data_loader import load_aicode_dataset, load_keystroke_dataset
+from .data_loader import load_code_llm_dataset
 
-__all__ = ["load_aicode_dataset", "load_keystroke_dataset"]
+__all__ = ["load_code_llm_dataset"]
